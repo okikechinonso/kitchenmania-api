35adef859b26157e14dae3fe4014482b2cfcc93c
Okike Chinonso <Chinonsookike@gmail.com>
2021-12-12 13:32:09 +0100

    working on signup

--- infrastructure/persistence/blog_db.go
+++ infrastructure/persistence/blog_db.go
@@ -3,6 +3,7 @@ package persistence
 
 import "kitchenmaniaapi/domain/entity"
 
-func(database *Database) CreatePost(user *entity.User){
-	
+func (d *Database) CreatePost(blog *entity.Blog) error {
+	result := d.DB.Create(blog)
+	return result.Error
 }
