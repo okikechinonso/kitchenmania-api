6605d7e26c53ef060d23355c538cdc8ae920ea45
Okike Chinonso <Chinonsookike@gmail.com>
2022-01-16 21:58:16 +0100

    creating blog post

--- application/handlers/create_blog_handler.go
+++ application/handlers/create_blog_handler.go
@@ -11,11 +11,29 @@ import (
 
 func (a *App) CreateBlog() gin.HandlerFunc{
 	return func(c *gin.Context){
+		userI,exist := c.Get("user");
+		if  !exist {
+			response.JSON(c,"can't get user from context",http.StatusInternalServerError,nil,"no user found")
+			return
+		}
+		user,ok := userI.(entity.User)
+		if !ok {
+			response.JSON(c,"",http.StatusInternalServerError,nil,"Unable to decode")
+			return
+		}
 		post :=&entity.Blog{}
 		err:= helpers.Decode(c,post) 
 		if err != nil {
 			response.JSON(c,"",http.StatusInternalServerError,nil,"Unable to decode"+err.Error())
+			return
 		}
+		
+		post.Title = c.PostForm("title")
+		post.Body  = c.PostForm("body")
+		post.Author = user.FirstName + user.LastName
+
+		form, err := c.MultipartForm()
+
 		
 	}
 }
