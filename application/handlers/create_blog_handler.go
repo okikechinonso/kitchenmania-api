224b08eca5fb827d5fd16d91fed979fa6ea9ac4a
Okike Chinonso <Chinonsookike@gmail.com>
2022-02-03 16:14:45 +0100

    modified the create blog handler

--- application/handlers/create_blog_handler.go
+++ application/handlers/create_blog_handler.go
@@ -11,6 +11,7 @@ import (
 	"strings"
 
 	"github.com/gin-gonic/gin"
+	"github.com/globalsign/mgo/bson"
 )
 
 func (a *App) CreateBlog() gin.HandlerFunc {
@@ -33,34 +34,38 @@ func (a *App) CreateBlog() gin.HandlerFunc {
 			return
 		}
 		images := form.File["images"]
-		for _, f := range images {
-			file, err := f.Open()
-			if err != nil {
-				response.JSON(c, "", http.StatusInternalServerError, nil, "No file multipart form found")
-				return
+		if len(images) != 0{
+			for _, f := range images {
+				file, err := f.Open()
+				if err != nil {
+					response.JSON(c, "", http.StatusInternalServerError, nil, "No file multipart form found")
+					return
+				}
+	
+				if _, ok = helpers.CheckSupportedFile(strings.ToLower(f.Filename)); ok {
+					log.Println(filepath.Ext(f.Filename))
+					response.JSON(c, "", http.StatusInternalServerError, nil, "invalid file extension")
+					return
+				}
+	
+				session, tempFileUrl, err := service.PreAWS(filepath.Ext(f.Filename), "blogphoto")
+				if err != nil {
+					response.JSON(c, "", http.StatusInternalServerError, nil, "unable to create AWS session"+err.Error())
+					return
+				}
+				url, err := service.UploadFileToS3(session, file, tempFileUrl, f.Size)
+				if err != nil {
+					response.JSON(c, "", http.StatusInternalServerError, nil, "unable to upload to File"+err.Error())
+					return
+				}
+				imgs := entity.Image{ImageUrl: url}
+				post.Images = append(post.Images, imgs)
 			}
-
-			if _, ok = helpers.CheckSupportedFile(strings.ToLower(f.Filename)); ok {
-				log.Println(filepath.Ext(f.Filename))
-				response.JSON(c, "", http.StatusInternalServerError, nil, "invalid file extension")
-				return
-			}
-
-			session, tempFileUrl, err := service.PreAWS(filepath.Ext(f.Filename), "blogphoto")
-			if err != nil {
-				response.JSON(c, "", http.StatusInternalServerError, nil, "unable to create AWS session"+err.Error())
-				return
-			}
-			url, err := service.UploadFileToS3(session, file, tempFileUrl, f.Size)
-			if err != nil {
-				response.JSON(c, "", http.StatusInternalServerError, nil, "unable to upload to File"+err.Error())
-				return
-			}
-			imgs := entity.Image{ImageUrl: url}
-			post.Images = append(post.Images, imgs)
 		}
+		
 
 		post.UserID = user.ID
+		post.Model.ID = string(bson.NewObjectId().Hex())
 		post.Author = user.FirstName + " " + user.LastName
 		post.Title = c.PostForm("title")
 		post.Body = c.PostForm("body")
