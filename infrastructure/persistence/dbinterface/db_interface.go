9a9f10c7175dbde599b9c664a8c492534e390293
Okike Chinonso <Chinonsookike@gmail.com>
2022-02-03 15:18:19 +0100

    moved the file location dbs

--- infrastructure/persistence/dbinterface/db_interface.go
+++ infrastructure/persistence/dbinterface/db_interface.go
@@ -5,6 +5,6 @@ import "kitchenmaniaapi/domain/entity"
 type DbInterface interface {
 	NewUser(user *entity.User) error
 	FindUserByEmail(email string) (*entity.User, error)
-	CreatePost(blog *entity.Blog) error
+	CreatePost(blog entity.Blog) error
 	TokenInBlacklist(token *string) bool
 }
--- infrastructure/persistence/pgsql/blog_db.go
+++
@@ -1,9 +0,0 @@
-// DB blog methods
-package pgsql
-
-import "kitchenmaniaapi/domain/entity"
-
-func (d *Database) CreatePost(blog *entity.Blog) error {
-	result := d.PgDB.Create(blog)
-	return result.Error
-}
