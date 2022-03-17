6124ee4e32416c028cc221a0c9b2881f8e58320c
Okike Chinonso <Chinonsookike@gmail.com>
2022-03-17 09:26:05 +0100

    added mongodb

--- domain/postgres_db/postgres.go
+++ domain/postgres_db/postgres.go
@@ -2,24 +2,29 @@ package postgresdb
 
 import (
 	"kitchenmaniaapi/domain/entity"
+	"kitchenmaniaapi/entity"
 
 	"github.com/gofrs/uuid"
 )
 
+// type UserRepository interface {
+// 	NewUser(user *entity.User) error
+// 	FindUserByEmail(email string) (*entity.User, error)
+// 	CreatePost(blog entity.Blog) error
+// 	TokenInBlacklist(token *string) bool
+// 	DeletePost(blogID, userID string) error
+// 	UpdatePost(blog entity.Blog) error
+// 	GetAllPosts(user entity.User) ([]entity.Blog, error)
+// }
+
 type UserRepository interface {
-	NewUser(user *entity.User) error
-	FindUserByEmail(email string) (*entity.User, error)
-	CreatePost(blog entity.Blog) error
-	TokenInBlacklist(token *string) bool
-	DeletePost(blogID, userID string) error
-	UpdatePost(blog entity.Blog) error
-	GetAllPosts(user entity.User) ([]entity.Blog, error)
+	GetAll() ([]entity.User, error)
+	GetByID(email string) (entity.User, error)
+	Add(user entity.User) error
+	Update(user entity.User) error
+	Delete(email string) error
 }
 
-type Repository interface {
-	GetAll() ([]aggregate.Product, error)
-	GetByID(id uuid.UUID) (aggregate.Product, error)
-	Add(product aggregate.Product) error
-	Update(product aggregate.Product) error
-	Delete(id uuid.UUID) error
+type BlacklistRepository interface{
+	TokenInBlacklist(token *string) bool
 }
